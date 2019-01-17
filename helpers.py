b7e77344753ea8df1c924b8fec98f8afd06099e0
j2ho <justinho515@gmail.com>
2019-01-16 20:39:20 -0800

    Moved stock retrieval functions out of main script into helpers.py

---
+++ helpers.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 16 19:13:25 2019
+
+@author: Justin Ho
+"""
+
+import requests
+import urllib.parse
+
+def lookup(symbol):
+    """Look up quote for symbol."""
+    
+    #Contact API
+    try:
+        response = requests.get(f"https://api.iextrading.com/1.0/stock/{urllib.parse.quote_plus(symbol)}/quote")
+        response.raise_for_status()
+    except requests.RequestException:
+        return None
+    
+    #Parse response
+    try:
+        quote = response.json()
+        return {
+                "name": quote["companyName"],
+                "price": float(quote["latestPrice"]),
+                "symbol": quote["symbol"]
+                }
+    except (KeyError, TypeError, ValueError):
+        return None
+          
+def usd(value):
+    """Format value as USD"""
+    return f"${value:,.2f}"    
