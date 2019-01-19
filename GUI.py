17c579a9acc830fe91bf1316ba5cd5d6b1bfc1c1
j2ho <justinho515@gmail.com>
2019-01-19 09:47:19 -0800

    Added button to add stock ticker symbol to current list

--- GUI.py
+++ GUI.py
@@ -11,6 +11,7 @@ try:
 except ImportError:
     #for Python3
     import tkinter as tk
+    from tkinter import messagebox
 
 class GUI:
     def __init__(self):
@@ -35,9 +36,9 @@ class GUI:
         self.root.grid_columnconfigure(0, weight=1)
         self.root.grid_columnconfigure(1, weight=1)
         self.root.grid_columnconfigure(2, weight=1)
-        self.top_row.grid(row=0, columnspan=2)
-        self.mid_row.grid(row=1, columnspan=2)
-        self.bot_row.grid(row=2, columnspan=2)
+        self.top_row.grid(row=0, columnspan=3)
+        self.mid_row.grid(row=1, columnspan=3)
+        self.bot_row.grid(row=2, columnspan=3)
         
         #create the widgets for the top row
         self.see_stock_list_button = tk.Button(self.top_row, \
@@ -52,15 +53,15 @@ class GUI:
         self.see_stock_list_button.grid(row=0, sticky='we')
         
         #create the widgets for the middle row
-        self.add_ticker_label = tk.Button(self.mid_row, text='Add Ticker: ', \
+        self.add_ticker_label = tk.Label(self.mid_row, text='Ticker Symbol: ', \
                                          font='Times 20')
         self.add_ticker_entry = tk.Entry(self.mid_row, justify='left', \
                                          font='Times 20', takefocus=1)
-        self.add_price_label = tk.Button(self.mid_row, text='Add Price: ', \
+        self.add_price_label = tk.Label(self.mid_row, text='Price: ', \
                                         font='Times 20', justify='left')
         self.add_price_entry = tk.Entry(self.mid_row, justify='left', \
                                          font='Times 20', takefocus=1)
-        self.percent_change_label = tk.Button(self.mid_row, font='Times 20', \
+        self.percent_change_label = tk.Label(self.mid_row, font='Times 20', \
                                              text='% Change to Notify: ', \
                                              justify='left')
         self.percent_change_entry = tk.Entry(self.mid_row, justify='left', \
@@ -75,27 +76,43 @@ class GUI:
         self.percent_change_entry.grid(row=2, column=1, sticky='we')
         
         #create the widgets for the bottom row
-        self.remove_ticker_button = tk.Button(self.bot_row, \
-                                              text='Remove Ticker',  \
+        self.add_ticker_button = tk.Button(self.bot_row, \
+                                              text='Add Ticker to List',  \
                                               font='Times 20', \
                                               activebackground='blue', bd=2, \
                                               bg='grey', fg='black', \
                                               justify='center', padx=1000, \
-                                              pady=25, highlightthickness=10                                            , \
+                                              pady=25, highlightthickness=10, \
+                                              highlightcolor='red')
+                                           
+        self.remove_ticker_button = tk.Button(self.bot_row, \
+                                              text='Remove Ticker from List',  \
+                                              font='Times 20', \
+                                              activebackground='blue', bd=2, \
+                                              bg='grey', fg='black', \
+                                              justify='center', padx=1000, \
+                                              pady=25, highlightthickness=10, \
                                               highlightcolor='red')
         
         #layout the widgets for the bottom row
-        self.remove_ticker_button.grid(row=0, sticky='we')
+        self.add_ticker_button.grid(row=0, sticky='we')
+        self.remove_ticker_button.grid(row=1, sticky='we')
         
         #allow user to tab to next widget
         self.see_stock_list_button.bind('<Tab>', focus_next_widget)
         self.add_ticker_entry.bind('<Tab>', focus_next_widget)
         self.add_price_entry.bind('<Tab>', focus_next_widget)
         self.percent_change_entry.bind('<Tab>', focus_next_widget)
+        self.add_ticker_button.bind('<Tab>', focus_next_widget)
         self.remove_ticker_button.bind('<Tab>', focus_next_widget)
         
-        #set initial focus to add_ticker_entry
-        self.add_ticker_entry.focus()   
+        #set initial focus to see_stock_list_button
+        self.see_stock_list_button.focus()   
+        
+        #add validation to Entry widgets
+#        def isOkay(self, )
+#        if len(self.add_ticker_entry.get()) == 0:
+#            tk.messagebox.showerror('USER ERROR', 'Enter a symbol!')
         
 #allow user to tab to next widget
 def focus_next_widget(event):
