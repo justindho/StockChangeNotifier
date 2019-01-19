c78941f6737994acc636f2924e817170d2456ab7
j2ho <justinho515@gmail.com>
2019-01-19 11:11:52 -0800

    User can now get current stock price via IEX API

--- GUI.py
+++ GUI.py
@@ -12,6 +12,8 @@ except ImportError:
     #for Python3
     import tkinter as tk
     from tkinter import messagebox
+    
+from helpers import lookup, usd
 
 class GUI:
     def __init__(self):
@@ -61,6 +63,15 @@ class GUI:
                                         font='Times 20', justify='left')
         self.add_price_entry = tk.Entry(self.mid_row, justify='left', \
                                          font='Times 20', takefocus=1)
+        self.get_current_price_button = tk.Button(self.mid_row, \
+                                                  command=self.lookup_current_price, \
+                                                  text='Get Current Price', \
+                                                  font='Times 20', bd=2, \
+                                                  activebackground='blue', \
+                                                  bg='grey', fg='black', \
+                                                  justify='center', padx=5, \
+                                                  highlightthickness=10, \
+                                                  highlightcolor='red')
         self.percent_change_label = tk.Label(self.mid_row, font='Times 20', \
                                              text='% Change to Notify: ', \
                                              justify='left')
@@ -72,6 +83,7 @@ class GUI:
         self.add_ticker_entry.grid(row=0, column=1, sticky='we')
         self.add_price_label.grid(row=1, column=0, sticky='we')
         self.add_price_entry.grid(row=1, column=1, sticky='we')
+        self.get_current_price_button.grid(row=1, column=2, sticky='we')
         self.percent_change_label.grid(row=2, column=0, sticky='we')
         self.percent_change_entry.grid(row=2, column=1, sticky='we')
         
@@ -102,19 +114,33 @@ class GUI:
         self.see_stock_list_button.bind('<Tab>', focus_next_widget)
         self.add_ticker_entry.bind('<Tab>', focus_next_widget)
         self.add_price_entry.bind('<Tab>', focus_next_widget)
+        self.get_current_price_button.bind('<Tab>', focus_next_widget)
         self.percent_change_entry.bind('<Tab>', focus_next_widget)
         self.add_ticker_button.bind('<Tab>', focus_next_widget)
         self.remove_ticker_button.bind('<Tab>', focus_next_widget)
         
         #set initial focus to see_stock_list_button
-        self.see_stock_list_button.focus()   
+        self.see_stock_list_button.focus()  
         
-        #add validation to Entry widgets
-#        def isOkay(self, )
+    def lookup_current_price(self):
+        """handle get_current_price_button button click to get stock price"""
+        symbol = self.add_ticker_entry.get()
+        self.add_price_entry.delete(0, 'end')
+        self.add_price_entry.insert(0, lookup(symbol)['price'])
+    
+        
+    #add validation to Entry widgets
+#    def isOkay(self, )
 #        if len(self.add_ticker_entry.get()) == 0:
 #            tk.messagebox.showerror('USER ERROR', 'Enter a symbol!')
         
-#allow user to tab to next widget
+        
+        
 def focus_next_widget(event):
+    """allow user to tab to next widget"""
     event.widget.tk_focusNext().focus()
     return('break')        
+    
+
+    
+    
